e4fd671c9c69c87f078145211718b3630d0ea6b6
Sachit Khadka <sachitkhadka73@gmail.com>
2024-01-21 20:04:10 +0545

    fix

--- example/views.py
+++ example/views.py
@@ -9,13 +9,13 @@ def index(request):
         city = request.POST['city']
 
         # Replace 'YOUR_RAPIDAPI_KEY' with your actual RapidAPI key
-        rapidapi_key = 'b91aba5284msh0f0015993f51486p1a54a1jsn961fc08fa6b8'
+        rapidapi_key = ''
 
         # Replace 'YOUR_OPENWEATHERMAP_HOST' with the correct host for the OpenWeatherMap RapidAPI endpoint
-        openweathermap_host = 'open-weather13.p.rapidapi.com'
+        openweathermap_host = ''
 
         # Construct the URL with the city name and API key
-        url = f'https://open-weather13.p.rapidapi.com/city/landon'
+        url = f'https://{openweathermap_host}/weather?q={city}'
 
         # Set up headers for the RapidAPI request
         headers = {
@@ -24,25 +24,36 @@ def index(request):
             'Accept': 'application/json',
         }
 
-        # Make the API request using urllib
-        request = urllib.request.Request(url, headers=headers)
-        response = urllib.request.urlopen(request)
-        source = response.read()
+        try:
+            # Make the API request using urllib
+            request = urllib.request.Request(url, headers=headers)
+            response = urllib.request.urlopen(request)
+            source = response.read()
 
-        # Parse the JSON data
-        list_of_data = json.loads(source)
+            # Parse the JSON data
+            list_of_data = json.loads(source)
 
-        data = {
-            "country_code": str(list_of_data['sys']['country']),
-            "coordinate": str(list_of_data['coord']['lon']) + ' ' + str(list_of_data['coord']['lat']),
-            "temp": str(list_of_data['main']['temp']) + 'k',
-            "pressure": str(list_of_data['main']['pressure']),
-            "humidity": str(list_of_data['main']['humidity']),
-        }
+            data = {
+                "country_code": str(list_of_data['sys']['country']),
+                "coordinate": str(list_of_data['coord']['lon']) + ' ' + str(list_of_data['coord']['lat']),
+                "temp": str(list_of_data['main']['temp']) + 'k',
+                "pressure": str(list_of_data['main']['pressure']),
+                "humidity": str(list_of_data['main']['humidity']),
+            }
 
-        print(data)
+            print(data)
+
+        except urllib.error.HTTPError as e:
+            # Handle HTTP errors (e.g., 404, 500)
+            print(f"HTTPError: {e}")
+            data = {}
+
+        except Exception as e:
+            # Handle other exceptions
+            print(f"Error: {e}")
+            data = {}
 
     else:
         data = {}
 
-    return render(request, 'example/main.html')
+    return render(request, 'example/main.html', {'data': data})
